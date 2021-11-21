3a200cbdd39dee4c55fc2d6a9baef62ac387cd8e
gdr22 <gdr@andrew.cmu.edu>
2021-11-21 12:53:15 -0500

    Added models and halfedge conversion code.

---
+++ src/load_halfedge.py
@@ -0,0 +1,25 @@
+import numpy as np
+import open3d as o3d
+
+data = "../data/bunny.ply"
+
+mesh = o3d.io.read_triangle_mesh(data, print_progress = True)
+
+halfedge_mesh = o3d.geometry.HalfEdgeTriangleMesh.create_from_triangle_mesh(mesh)
+
+verts = np.asarray(halfedge_mesh.vertices)
+halfedges = halfedge_mesh.half_edges
+
+print("Verts: %d" % len(verts))
+print("HalfEdges: %d" % len(halfedges))
+
+f = open("../data/bunny_halfedge.txt", "w")
+f.write("%d %d\n" % (len(verts), len(halfedges)))
+
+for vert in verts:
+    f.write("%f %f %f\n" % (vert[0], vert[1], vert[2]))
+
+for halfedge in halfedges:
+    f.write("%d %d %d %d\n" % (halfedge.vertex_indices[0], halfedge.triangle_index, halfedge.next, halfedge.twin))
+
+f.close()
