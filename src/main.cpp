43a816cd862d0d59063f4864a10cfd6173f42e1e
gdr22 <gdr@andrew.cmu.edu>
2021-11-21 19:34:35 -0500

    Added code to read in the halfedge file.

---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string>
+#include "mesh_simplify.h"
+
+//void saxpyCuda(int N, float alpha, float* x, float* y, float* result);
+//void printCudaInfo();
+
+mesh_t load_mesh(std::string filename) {
+    printf("Loading mesh %s\n", filename.c_str());
+
+    FILE *input = fopen(filename.c_str(), "r");
+
+    mesh_t mesh;
+
+    // Read in the size of this file
+    fscanf(input, "%d %d\n", &mesh.vertex_cnt, &mesh.halfedge_cnt);
+
+    mesh.vertices  = (float*)calloc(mesh.vertex_cnt, sizeof(float) * 3);
+    mesh.halfedges = (int*)calloc(mesh.halfedge_cnt, sizeof(int) * 4);
+
+    // Read the vertex information from file
+    for (int i = 0; i < mesh.vertex_cnt; i++) {
+        float x, y, z;
+        fscanf(input, "%f %f %f\n", &x, &y, &z);
+
+        mesh.vertices[i * 3 + 0] = x;
+        mesh.vertices[i * 3 + 1] = y;
+        mesh.vertices[i * 3 + 2] = z;
+    }
+
+    mesh.triangle_cnt = 0;
+
+    // Read the halfedge information from file
+    for (int i = 0; i < mesh.halfedge_cnt; i++) {
+        int vertex, triangle, next, twin;
+        fscanf(input, "%d %d %d %d\n", &vertex, &triangle, &next, &twin);
+
+        mesh.halfedges[i * 4 + 0] = vertex;
+        mesh.halfedges[i * 4 + 1] = triangle;
+        mesh.halfedges[i * 4 + 2] = next;
+        mesh.halfedges[i * 4 + 3] = twin;
+
+        mesh.triangle_cnt = std::max(mesh.triangle_cnt, triangle);
+    }
+
+    printf("Verts: %d\tHalfedges %d\tTriangles: %d\n", 
+           mesh.vertex_cnt, mesh.halfedge_cnt, mesh.triangle_cnt);
+
+    fclose(input);
+
+    return mesh;
+}
+
+
+void usage(const char* progname) {
+    printf("Usage: %s [options]\n", progname);
+    printf("Program Options:\n");
+    printf("  -n  --arraysize <INT>  Number of elements in arrays\n");
+    printf("  -?  --help             This message\n");
+}
+
+
+int main(int argc, char** argv)
+{
+
+    mesh_t mesh = load_mesh("/mnt/d/Documents/Classes/15-418/Final Project/data/bunny_halfedge.txt");
+
+    for(int i = 0; i < mesh.vertex_cnt; i++) {
+        printf("%f %f %f\n", 
+                mesh.vertices[i * 3 + 0],
+                mesh.vertices[i * 3 + 1],
+                mesh.vertices[i * 3 + 2]);
+    }
+
+    
+    for(int i = 0; i < mesh.halfedge_cnt; i++) {
+        printf("%d %d %d %d\n", 
+                mesh.halfedges[i * 4 + 0],
+                mesh.halfedges[i * 4 + 1],
+                mesh.halfedges[i * 4 + 2],
+                mesh.halfedges[i * 4 + 3]);
+    }
+
+    //printCudaInfo();
+
+    return 0;
+}
--- src/mesh_simplify.h
+++ src/mesh_simplify.h
@@ -4,3 +4,14 @@ typedef struct {
     float z;
 } vertex_t;
 
+typedef struct
+{
+    int vertex_cnt;
+    int halfedge_cnt;
+    int triangle_cnt;
+
+    //float3* vertices;
+    //int4* halfedges;
+    float* vertices;
+    int* halfedges;
+} mesh_t;
